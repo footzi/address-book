d056d73bee0a06458680d578ed67e38cc943cc94
footzi <footzi@mail.ru>
2017-10-12 11:03:24 +0300

    to deploy

--- js/main.js
+++ js/main.js
@@ -6,11 +6,7 @@ $("#get").on("click", () => {
         success: data => {
             node(data);
         }
-<<<<<<< HEAD
     });
-=======
-    })
->>>>>>> d81875065de9f3bdc2d5ab9eb1dec7b1c5e1d402
 });
 
 $("#send").on("click", () => {
@@ -71,10 +67,6 @@ const node = (data) => {
 
         document.getElementById("line-" + i).appendChild(td);
         document.querySelector("#line-" + i).querySelector("td:last-child").appendChild(btn);
-<<<<<<< HEAD
-=======
-
->>>>>>> d81875065de9f3bdc2d5ab9eb1dec7b1c5e1d402
 
 
     
@@ -102,7 +94,6 @@ const node = (data) => {
             }
         })
     })
-<<<<<<< HEAD
 }
 
 
@@ -112,6 +103,3 @@ $(".about__buttons_open").on("click", function(){
 $(".about__buttons_сlose").on("click", function(){
 		$(".about__text").fadeOut();
 	})
-=======
-}
->>>>>>> d81875065de9f3bdc2d5ab9eb1dec7b1c5e1d402
